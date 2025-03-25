184a87422b7457f12a56c5f8e73f061cf07a00d7
joviedev <sinjing81@gmail.com>
2025-03-25 15:34:11 +1100

    Header Full Functional and Landing Page Access

---
+++ src/Component/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div>
+      {/* Your login form here */}
+      Login Page
+    </div>
+  );
+};
+
+export default SignUp;
---
+++ src/FixedComponent/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+      {/* Your login form here */}
+      Login Page
+    </div>
+  );
+};
+
+export default NavBar;
